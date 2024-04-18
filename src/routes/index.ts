f3d8a0dfe953457e1aa25e83a33c1a4f231bea93
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2024-04-17 23:07:56 -0300

    refactor routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -12,184 +12,164 @@ const api = new Hono({
     router: new SmartRouter({
         routers: [new RegExpRouter(), new TrieRouter()],
     })
-})
-api.use('*', cors({ origin: ['http://localhost:3000', 'https://localhost:3000'], }))
+});
 
+// Middleware para manejar CORS
+api.use('*', cors({ origin: ['http://localhost:3000', 'https://localhost:3000'] }));
 
-// Update ProductDayly fluctuations
+// Actualizar fluctuaciones diarias de productos
 api.post('products/update', async (c) => {
-    // Find the last daylyPrice for all products and only filter each which the last day of update is was yesterday or before
-    const lastDaylyPrices = await db.productDailyPrice.groupBy({
-        by: ['productId'],
-        _max: {
-            date: true
-        },
-    })
-    
-    // [
-    //     {
-    //       "_max": {
-    //         "date": "2024-04-17T17:41:57.220Z"
-    //       },
-    //       "productId": "https://www.cotodigital3.com.ar/sitios/cdigi/producto/-scones-sconcito-9-de-oro-bsa-200-grm/_/A-00089952-00089952-200"
-    //     },
-    //     {
-    //       "_max": {
-    //         "date": "2024-04-17T17:38:27.388Z"
-    //       },
-    //       "productId": "https://www.cotodigital3.com.ar/sitios/cdigi/producto/-bizcochos-grasa-9-de-oro-paq-200-grm/_/A-00004606-00004606-200"
-    //     },
-    //     {
-    //       "_max": {
-    //         "date": "2024-04-17T13:57:14.002Z"
-    //       },
-    //       "productId": "https://www.carrefour.com.ar/fideos-spaghettini-barilla-n3-500-g/p"
-    //     },]
-
-    // example: if today is 18 and the last product update was 17 or before, we need to keep the productId for update now
-    const productsToUpdate = lastDaylyPrices.filter(daylyPrice => {
-        const lastDaylyPriceDate = new Date(daylyPrice._max.date as Date)
-        const today = new Date()
-        today.setDate(today.getDate() - 1)
-        return lastDaylyPriceDate < today
-    })
-
-    const promises = productsToUpdate.map(async product => {
-        const supermarket = product.productId.includes('carrefour') ? 'carrefour' : 'coto';
-        const url = product.productId;
-    
-        if (supermarket === 'carrefour') {
-            try {
-                const productData = await getProductDataCarrefour(url);
-                return productData;
-            } catch (e) {
-                // Manejo de errores
-                console.error('Error en getProductDataCarrefour:', e);
-                return null;
-            }
-        }
-    
-        if (supermarket === 'coto') {
-            try {
-                const productData = await getProductDataCoto(url);
-                return productData;
-            } catch (e) {
-                // Manejo de errores
-                console.error('Error en getProductDataCoto:', e);
-                return null;
-            }
-        }
-    
-        return null;
-    });
-
-    const updates = await Promise.all(promises)
-    
     try {
+        const currentTime = new Date();
+        const allowedTime = new Date(currentTime);
+        allowedTime.setHours(8, 15, 0, 0); // Establecer la hora permitida a las 8:15 AM
+
+        if (currentTime < allowedTime) {
+            c.status(403);
+            return c.json({ error: 'Forbidden: Update can only be performed after 8:15 AM' });
+        }
+
+        const lastDaylyPrices = await db.productDailyPrice.groupBy({
+            by: ['productId'],
+            _max: {
+                date: true
+            },
+        });
+
+        const today = new Date();
+        today.setHours(0, 0, 0, 0); // Establecer la hora a 00:00:00
+
+        const yesterday = new Date(today);
+        yesterday.setDate(today.getDate() - 1);
+
+        const productsToUpdate = lastDaylyPrices.filter(daylyPrice => {
+            const lastDaylyPriceDate = new Date(daylyPrice._max.date as Date);
+            lastDaylyPriceDate.setHours(0, 0, 0, 0); // Establecer la hora a 00:00:00
+            return lastDaylyPriceDate < yesterday;
+        });
+        
+        if (productsToUpdate.length === 0) {
+            c.status(409);
+            return c.json({ error: 'No products to update' });
+        }
+
+        const updates = await Promise.all(productsToUpdate.map(async product => {
+            const supermarket = product.productId.includes('carrefour') ? 'carrefour' : 'coto';
+            const url = product.productId;
+        
+            try {
+                if (supermarket === 'carrefour') {
+                    return await getProductDataCarrefour(url);
+                } else if (supermarket === 'coto') {
+                    return await getProductDataCoto(url);
+                }
+            } catch (e) {
+                console.error(`Error en getProductData (${supermarket}):`, e);
+                return null;
+            }
+        }));
+
         await db.productDailyPrice.createMany({
-            data: [
-                ...updates.map(update => {
-                    return {
-                        productId: update?.pid as string,
-                        hasPromotion: Boolean(update?.hasPromotion),
-                        price: update?.realPrice as number,
-                        promoPrice: update?.promoPrice as number,
-                        date: new Date()
-                    }
-                })
-            ]
-        })
-        return c.json({ data: productsToUpdate })
-    }catch(e: any){
-        c.status(500)
-        return c.json({
-            error: e.message
-        })
-    }
-})
+            data: updates.filter(update => update !== null).map(update => ({
+                productId: update?.pid as string,
+                hasPromotion: Boolean(update?.hasPromotion),
+                price: update?.realPrice as number,
+                promoPrice: update?.promoPrice as number,
+                date: new Date()
+            }))
+        });
 
-// Get all products
+        return c.json({ data: productsToUpdate });
+    } catch (e) {
+        console.error('Error en la actualización de productos:', e);
+        c.status(500);
+        return c.json({ error: 'Internal server error' });
+    }
+});
+
+
+// Obtener todos los productos
 api.get('products', async (c) => {
-    const products = await db.product.findMany({
-        include: {
-            dailyPrices: true
-        }
-    })
-    // filled products
-    return c.json({
-        data: await Promise.all(products.map(async product => await productFillData(product)))
-    })
-})
-
-// Get only one product
-api.post('product/search', async (c) => {
-    const productId = (await c.req.json())?.pid
-    if (!productId) {
-        c.status(400)
-        return c.json({
-            error: 'Product ID is required'
-        })
-    }
     try {
+        const products = await db.product.findMany({
+            include: {
+                dailyPrices: true
+            }
+        });
+
+        const filledProducts = await Promise.all(products.map(async product => await productFillData(product)));
+
+        return c.json({ data: filledProducts });
+    } catch (e) {
+        console.error('Error al obtener productos:', e);
+        c.status(500);
+        return c.json({ error: 'Internal server error' });
+    }
+});
+
+// Buscar un solo producto
+api.post('product/search', async (c) => {
+    try {
+        const { pid } = await c.req.json();
+        if (!pid) {
+            c.status(400);
+            return c.json({ error: 'Product ID is required' });
+        }
+
         const product = await db.product.findFirstOrThrow({
             where: {
-                productId: productId
+                productId: pid
             },
             include: {
                 dailyPrices: true
             }
-        })
-        return c.json({
-            data: await productFillData(product)
-        })
-    } catch (e: any) {
-        console.log(e.message)
-        c.status(404)
-        return c.json({
-            error: "Producto no encontrado"
-        })
-    }
-})
+        });
 
-// Save product first time
+        return c.json({ data: await productFillData(product) });
+    } catch (e) {
+        console.error('Error al buscar un producto:', e);
+        c.status(404);
+        return c.json({ error: 'Producto no encontrado' });
+    }
+});
+
+// Guardar un producto por primera vez
 api.post('product', async (c) => {
-    const body = await c.req.json()
-
-    const toSave = {
-        productId: body.pid,
-        title: body.title,
-        supermarket: body.supermercado,
-        url: body.productUrl,
-        imageUrl: body.imageUrl,
-        category: body.category,
-    }
-
-    const toSaveFirstDailyPrice = {
-        productId: body.pid,
-        hasPromotion: body.hasPromotion === 'on' ? true : false,
-        price: parseFloat(body.realPrice),
-        promoPrice: parseFloat(body.promoPrice),
-    }
-
     try {
+        const { pid, title, supermercado: supermarket, productUrl, imageUrl, category, hasPromotion, realPrice, promoPrice } = await c.req.json();
+
         const [product, dailyPrice] = await db.$transaction([
-            db.product.create({ data: toSave }),
-            db.productDailyPrice.create({ data: toSaveFirstDailyPrice }),
-        ])
+            db.product.create({
+                data: {
+                    productId: pid,
+                    title,
+                    supermarket,
+                    url: productUrl,
+                    imageUrl,
+                    category,
+                }
+            }),
+            db.productDailyPrice.create({
+                data: {
+                    productId: pid,
+                    hasPromotion: hasPromotion === 'on' ? true : false,
+                    price: parseFloat(realPrice),
+                    promoPrice: parseFloat(promoPrice),
+                }
+            }),
+        ]);
+
         return c.json({
             data: await productFillData({
                 ...product,
                 dailyPrices: [dailyPrice]
             })
-        })
-    } catch (e: any) {
-        console.log(e.message)
-        c.status(400)
-        return c.json({
-            error: e.message
-        })
+        });
+    } catch (e) {
+        console.error('Error al guardar un producto:', e);
+        c.status(400);
+        return c.json({ error: 'Bad request' });
     }
-})
+});
 
-
-export default api
+export default api;
