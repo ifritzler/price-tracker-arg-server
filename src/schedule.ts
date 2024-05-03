bd542859bbb41f88f8fc4d3c24c6cd000899e688
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2024-05-03 12:36:38 -0300

    fix cron job to work with a free hosting that sleeps often

--- src/schedule.ts
+++ src/schedule.ts
@@ -1,12 +1,17 @@
 import { CronJob } from "cron";
 import { updateProductFluctuations } from "./services/fluctuations.js";
+import { db } from "./database/prisma.js";
 
 export function createJobToUpdateProductsEachDay() {
+    
     const job = CronJob.from({
         cronTime: '00 00 11 * * *', // All days all months each 11am o'clock in the morning
         onTick: async () => {
             try {
-                const { error, message } = await updateProductFluctuations();
+                if(new Date().getUTCHours() - 3 < 11) {
+                    throw new Error("Updates only after 11 am.")
+                }
+                const { error, message } = await updateProductFluctuations()
                 console.info({error, message})
             } catch(error: any) {
                 console.error(error.message)
