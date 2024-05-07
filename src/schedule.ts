61613284bc282d21872f77aa857dcef427f1ab25
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2024-05-07 00:36:52 -0300

    add monitoring values at start server

--- src/schedule.ts
+++ src/schedule.ts
@@ -1,7 +1,11 @@
 import { CronJob } from 'cron'
 import { updateProductFluctuations } from './services/fluctuations.js'
+import { getOnlyDateWithoutHours } from './utils/date.js'
 
 export function createJobToUpdateProductsEachDay() {
+  console.log("Hora de servidor en UTC: " + new Date().getUTCHours())
+  console.log("getOnlyDateWithoutHours: ", getOnlyDateWithoutHours())
+  
   const job = CronJob.from({
     cronTime: '00 00 11 * * *', // All days all months each 11am o'clock in the morning
     onTick: async () => {
