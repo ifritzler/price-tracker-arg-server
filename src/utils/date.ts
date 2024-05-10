99c5cd3bf9af9d176b49b2635a7e9ce79a86f904
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2024-05-09 22:30:32 -0300

    add flag to avoid inserts while testint dates

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,30 +1,32 @@
-
-export function getOnlyDateWithoutHours() {
-  const date = getActualHourBuenosAires()
-  const hour = Number(
-    new Date()
-      .toLocaleTimeString('en-US', {
-        timeZone: 'America/Argentina/Buenos_Aires',
-        hourCycle: "h24"
-      })
-      .split(':')[0],
-  )
-  
-  if (hour < 11) {
-    date.setHours(-27, 0, 0, 0)
-    const dateModified = new Date(date)
-    return dateModified
-  }
-
-  date.setHours(-3, 0, 0, 0)
-  const dateModified = new Date(date)
-  return dateModified
+export function getActualHourBuenosAires() {
+  const date = new Date();
+  const options = { timeZone: 'America/Argentina/Buenos_Aires' };
+  const localDateString = date.toLocaleString('en-US', options);
+  const localDate = new Date(localDateString);
+  return localDate;
 }
 
-export function getActualHourBuenosAires() {
-  const date = new Date()
-  const options = { timeZone: 'America/Argentina/Buenos_Aires' }
-  const localDateString = date.toLocaleString('en-US', options)
-  const localDate = new Date(localDateString)
-  return localDate
+export function resetToMidnight(date: Date) {
+  const midnight = new Date(date);
+  midnight.setHours(0, 0, 0, 0); // Establecer la hora a medianoche en la zona horaria local
+  const offset = date.getTimezoneOffset(); // Obtener el desfase horario de la zona horaria local
+  midnight.setMinutes(midnight.getMinutes() - offset); // Ajustar para el desfase horario
+  return midnight;
+}
+
+
+export function getOnlyDateWithoutHours() {
+  // Obtener la hora actual de Buenos Aires
+  const date = getActualHourBuenosAires();
+  const hour = date.getHours();
+
+  // Determinar si la hora actual es antes de las 11AM en Buenos Aires
+  if (hour < 11) {
+    // Si es antes de las 11AM, restar 1 día
+    date.setDate(date.getDate() - 1);
+  }
+
+  // Resetear la hora a la medianoche
+  const dateWithoutHours = resetToMidnight(date);
+  return dateWithoutHours;
 }
